0d44d7921aaceb25f97ef7b00064d1640e3253d0
Jegan A <jegansignup@gmail.com>
2024-08-16 14:44:03 +0530

    made some changes

--- aws-terraform/vpc/main.tf
+++ aws-terraform/vpc/main.tf
@@ -1,17 +1,13 @@
-provider "aws" {
-    region = var.region
-  
-}
-data "aws_availability_zones" "aws_az" {}
+data "aws_availability_zones" "this" {}
 
 locals {
-  azs                 = slice(data.aws_availability_zones.aws_az.names, 0, 2)
+  azs                 = slice(data.aws_availability_zones.this.names, 0, 2)
   public_subnet_cidr  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k * 2)]
   private_subnet_cidr = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k * 2 + 1)]
 }
 
 # VPC creation
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr
   tags = {
     Name = "${var.resource_name}-vpc"
@@ -21,7 +17,7 @@ resource "aws_vpc" "vpc" {
 # Public subnet creation
 resource "aws_subnet" "public_subnet" {
   count             = length(local.public_subnet_cidr)
-  vpc_id            = aws_vpc.vpc.id
+  vpc_id            = aws_vpc.this.id
   cidr_block        = element(local.public_subnet_cidr, count.index)
   availability_zone = element(local.azs,count.index)
   tags = {
@@ -32,7 +28,7 @@ resource "aws_subnet" "public_subnet" {
 # Private subnet creation
 resource "aws_subnet" "private_subnet" {
     count             = length(local.private_subnet_cidr)
-    vpc_id            = aws_vpc.vpc.id
+    vpc_id            = aws_vpc.this.id
     cidr_block        = element(local.private_subnet_cidr, count.index)
     availability_zone = element(local.azs,count.index)
     tags = {
@@ -42,7 +38,7 @@ resource "aws_subnet" "private_subnet" {
 
 # Internet gateway creation
 resource "aws_internet_gateway" "internet_gw" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.this.id
   tags = {
     Name = "${var.resource_name}-internet-gw"
     
@@ -51,7 +47,7 @@ resource "aws_internet_gateway" "internet_gw" {
 
 # Public subnet route table and association
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.this.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet_gw.id
@@ -70,14 +66,14 @@ resource "aws_route_table_association" "public_route_table_association" {
 
 # Private subnet route table and association
 resource "aws_route_table" "private_route_table_association" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.this.id
   tags = {
     Name = "${var.resource_name}-private-subnet-route"
     
   }
 }
 
-resource "aws_route_table_association" "dev_proj_1_private_rt_subnet_association" {
+resource "aws_route_table_association" "private_route_table_association" {
   count          = length(local.private_subnet_cidr)
   subnet_id      = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.private_route_table_association.id
